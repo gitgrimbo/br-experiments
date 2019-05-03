3e0ec5ef6a99d47b79d907898b2a562b3ceeed1f
gitgrimbo <gitgrimbo@gmail.com>
2019-05-03 20:01:33 +0100

    don't log so much SVG data

--- experiments/image-generator/svg.js
+++ experiments/image-generator/svg.js
@@ -28,11 +28,11 @@ function parseSVG(svg) {
 function getElementValue(el) {
   switch (el.tagName.toLowerCase()) {
     case "image": {
-      console.log(el);
+      // console.log(el);
       const href = el.getAttribute("xlink:href");
-      console.log(href);
+      // console.log(href);
       const isDataUrl = href.startsWith("data:image/");
-      console.log(isDataUrl);
+      // console.log(isDataUrl);
       return isDataUrl ? null : href;
     }
     default: return el.textContent;
