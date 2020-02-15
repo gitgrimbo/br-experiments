70f276e38be4e3330ba4e1a9a453214c98fdb7d9
gitgrimbo <gitgrimbo@gmail.com>
2020-02-15 22:23:28 +0000

    web: integrate with new AWS image-generator

--- experiments/image-generator/createPNG.js
+++ experiments/image-generator/createPNG.js
@@ -1,20 +1,29 @@
+import { encode as Base64Encode } from "./base64";
+
 const convertImageEndpointLocal = "http://localhost:9902/";
-const convertImageEndpointProd = "https://us-central1-bladerunners.cloudfunctions.net/convertImage";
-const screenshotEndpointLocal = "http://localhost:9901/";
-const screenshotEndpointProd = "https://us-central1-bladerunners.cloudfunctions.net/screenshot";
+const convertImageEndpointProd = "https://us-central1-bladerunners.cloudfunctions.net/convertImage/";
+//const screenshotEndpointLocal = "http://localhost:9901/";
+const screenshotEndpointLocal = "http://localhost:3000/prod/screenshot/";
+//const screenshotEndpointProd = "https://us-central1-bladerunners.cloudfunctions.net/screenshot/";
+const screenshotEndpointProd = "https://k16z8cm9r9.execute-api.eu-west-2.amazonaws.com/prod/screenshot/";
 
 export default async function createPNG(svgSource) {
   const convertImageEndpoint = screenshotEndpointProd;
 
   try {
-    const formData = new FormData();
-    formData.append("image.name", "image.svg");
-    formData.append("image.type", "svg");
-    formData.append("image.data", svgSource);
+    const s = Base64Encode(svgSource);
+    const data = {
+      "image.name": "image.svg",
+      "image.type": "svg",
+      "image.data": s,
+    };
 
     const resp = await fetch(convertImageEndpoint, {
       method: "POST",
-      body: formData,
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     if (!resp.ok) {
@@ -27,9 +36,10 @@ export default async function createPNG(svgSource) {
       throw err;
     }
 
-    const blob = await resp.blob();
-    return URL.createObjectURL(blob);
+    const base64image = await resp.text();
+    return "data:image/png;base64," + base64image;
   } catch (err) {
+    console.error(err);
     err.createPNG = {
       ...err.createPNG,
       endpoint: convertImageEndpoint,
